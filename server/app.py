02bea2bfd28a8b877ab2372edc77a55717f8c314
Dennis Demir <dennisjdemir@gmail.com>
2023-09-07 14:34:15 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -19,16 +19,57 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    if not animal:
+        make_response('<h1>404 animal not found</h1>', 404)
+    response_body =f'''
+            <ul>ID: {animal.id}</ul>
+            <ul>Name: {animal.name}</ul>
+            <ul>Species: {animal.species}</ul>
+            <ul>Zookeeper: {animal.zookeeper}</ul>
+            <ul>Enclosure: {animal.enclosure}</ul>
+            '''
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if not zookeeper:
+        return make_response('<h1>404 zookeeper not found</h1>', 404)
+    
+    animals =[animal for animal in zookeeper.animals]
+    response_body = f'''
+                        <ul>ID: {zookeeper.id}</ul>
+                        <ul>Name: {zookeeper.name}</ul>
+                        <ul>Birthday: {zookeeper.birthday}</ul>   
+                '''
+    for animal in animals:
+        response_body += f'<ul>Animal: {animal.name}'
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    
+    if not enclosure:
+        return make_response('<h1>404 enclosure not found</h1>', 404)
+    
+    animals = [animal for animal in enclosure.animals]
+    response_body = f'''
+                    <ul>ID: {enclosure.id}</ul>
+                    <ul>Environment: {enclosure.environment}</ul>
+                    <ul>Open To Visitors: {enclosure.open_to_visitors}</ul>
+                    '''
+    
+    if not animals:
+        response_body += f'<ul>No animals found</ul>'
+    else:
+        for animal in animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+    response = make_response(response_body, 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
